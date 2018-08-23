fdc55fcbf180a40e0c341f7ba0548c7fcead3509
Nicolas Marini <nmarini44@gmail.com>
2018-08-23 01:28:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY(books.year); "
+  "SELECT books.title, books.year
+    FROM books
+    WHERE books.series_id = 1
+    ORDER BY(books.year); "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -9,18 +12,18 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species)
-  FROM characters
-  GROUP BY(characters.species)
-  ORDER BY(COUNT(characters.species)) DESC
-  LIMIT 1;"
+    FROM characters
+    GROUP BY(characters.species)
+    ORDER BY(COUNT(characters.species)) DESC
+    LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors
-  JOIN series ON authors.id = series.author_id
-  JOIN subgenres ON series.subgenre_id = subgenres.id
-  GROUP BY(authors.name);"
+    FROM authors
+    JOIN series ON authors.id = series.author_id
+    JOIN subgenres ON series.subgenre_id = subgenres.id
+    GROUP BY(authors.name);"
 end
 
 def select_series_title_with_most_human_characters
