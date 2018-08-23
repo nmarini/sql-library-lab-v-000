5a10229d1b941842775ae6b2cf62d4d6b4b7ba04
Nicolas Marini <nmarini44@gmail.com>
2018-08-23 01:22:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title, COUNT(characters.*) FROM series JOIN book ON series.id = books.series_id JOIN character_books ON books.id = character_books.book_id
+  JOIN characters ON character_books.character_id = character.id GROUP BY() "
 end
 
 def select_character_names_and_number_of_books_they_are_in
