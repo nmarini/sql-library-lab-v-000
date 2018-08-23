5f715f1e858f3e0e13b077cc80a83db73687905f
Nicolas Marini <nmarini44@gmail.com>
2018-08-23 00:25:10 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,15 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
-  author_id INTEGER, 
-  sub-genre_id INTEGER
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  
 );
