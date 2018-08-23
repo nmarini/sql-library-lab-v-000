5e2df87752a13f00d4121f7d91b7aa12d20c7797
Nicolas Marini <nmarini44@gmail.com>
2018-08-23 01:27:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,14 +8,18 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY(characters.species) ORDER BY(COUNT(characters.species)) DESC LIMIT 1;"
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  GROUP BY(characters.species)
+  ORDER BY(COUNT(characters.species)) DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name 
-  FROM authors 
-  JOIN series ON authors.id = series.author_id 
-  JOIN subgenres ON series.subgenre_id = subgenres.id 
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON series.subgenre_id = subgenres.id
   GROUP BY(authors.name);"
 end
 
